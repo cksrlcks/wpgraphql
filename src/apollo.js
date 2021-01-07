62737b2f1949e4219602855b78641b5be084b269
cksrlcks <chanki.kim89@gmail.com>
2021-01-07 09:59:09 +0900

    https

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: "http://cksrlcks.cafe24.com/wp/graphql"
+    uri: "https://cksrlcks.cafe24.com/wp/graphql"
 })
 
 export default client;
