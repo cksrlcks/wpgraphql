87aeeb4105c0053ea36660b57a656cfe26de95eb
cksrlcks <chanki.kim89@gmail.com>
2021-01-07 09:57:40 +0900

    Updates

---
+++ precache-manifest.64c32cf795567088335c8ab3b06ea184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bf9f79efd168b5aebd190ca7dfe467",
+    "url": "/wpgraphql/index.html"
+  },
+  {
+    "revision": "e8025e5af409f3cd0e6a",
+    "url": "/wpgraphql/static/css/main.1641fc8e.chunk.css"
+  },
+  {
+    "revision": "e0936fa32303c7577b94",
+    "url": "/wpgraphql/static/js/2.093d0619.chunk.js"
+  },
+  {
+    "revision": "b5ff4111ab81c8c91e9e746653eb6fff",
+    "url": "/wpgraphql/static/js/2.093d0619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8025e5af409f3cd0e6a",
+    "url": "/wpgraphql/static/js/main.d76d9c38.chunk.js"
+  },
+  {
+    "revision": "8d82923ed6ecc33a2e27",
+    "url": "/wpgraphql/static/js/runtime-main.2b17c0bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wp-graphql/precache-manifest.608cd1cba5c522850e8bd3de160b8660.js"
+  "/wpgraphql/precache-manifest.64c32cf795567088335c8ab3b06ea184.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wp-graphql/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wpgraphql/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
